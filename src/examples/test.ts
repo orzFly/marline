9e2951dee77656289d60ef96e8441e559b93fdab
Yeechan Lu <wz.bluesnow@gmail.com>
2019-03-01 11:13:10 +0800

    Move test

--- src/examples/test.ts
+++ src/examples/test.ts
@@ -1,21 +1,30 @@
-import Navybird from "navybird";
-import { Marline } from "./index";
+import { Marline } from "..";
+
+async function delay(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), ms);
+  })
+}
 
 async function main() {
-  const marline = new Marline({ marginBottom: 5 });
+  const marline = new Marline({
+    marginTop: 0,
+    marginBottom: 5
+  });
+
   const interval = setInterval(() => {
     marline.bufferBottom.fill(String(Date.now()))
     marline.redraw();
   }, 100);
 
-  await Navybird.delay(100);
+  await delay(100);
   for (let i = 0; i < 100; i++) {
     if (i % 5 === 0) {
       if (marline.started) marline.stop()
       else marline.start()
     }
     console.log(i);
-    await Navybird.delay(1000);
+    await delay(1000);
   }
 
   clearInterval(interval);
